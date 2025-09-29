b06e26324d8c821d34752a9537ef8905c8d6885e
granthiparmeet-gif <granthi.parmeet@gmail.com>
2025-09-29 17:03:14 +0530

    This is the square root function

---
+++ implementation/square_root.py
@@ -0,0 +1,13 @@
+# x = float (input("Enter the number to find the square root: "))
+
+# print("The suare root of the given number is : ", x**0.5)
+
+#using upto 2 digit logic
+
+# x = float (input("Enter the number to find the square root: "))
+
+# print(f"The square root is {x**0.5 :.2f}")
+
+#Using The Lambda Function
+x = lambda x:x**0.5
+print(f"The square root is {x(67):.2f}")
