3ee1892f33d119e41465567a9efbc1b4058b46a8
granthiparmeet-gif <granthi.parmeet@gmail.com>
2025-09-30 15:33:39 +0530

    Adding a few more programs

--- implementation/square_root.py
+++ implementation/square_root.py
@@ -1,13 +1,33 @@
-# x = float (input("Enter the number to find the square root: "))
+# # x = float (input("Enter the number to find the square root: "))
 
-# print("The suare root of the given number is : ", x**0.5)
+# # print("The suare root of the given number is : ", x**0.5)
 
-#using upto 2 digit logic
+# #using upto 2 digit logic
 
-# x = float (input("Enter the number to find the square root: "))
+# # x = float (input("Enter the number to find the square root: "))
 
-# print(f"The square root is {x**0.5 :.2f}")
+# # print(f"The square root is {x**0.5 :.2f}")
 
-#Using The Lambda Function
-x = lambda x:x**0.5
-print(f"The square root is {x(67):.2f}")
+# # #Using The Lambda Function
+# # x = lambda x:x**0.5
+# # print(f"The square root is {x(67):.2f}")
+
+
+# # import math
+
+# # x = int(input("Enter te number you wish to find the sqaue root of : "))
+
+
+
+# # print(f"The square root is {math.sqrt(x) :.2f}")
+
+# x = 5
+# print(type(x))
+
+# name="Parmeet"
+# name1=3
+# name-3= 3
+
+x=y=z="Hello World"
+
+print(x,y,z)
