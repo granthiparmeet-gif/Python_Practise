0235afc2fc14ec781a3c2d1417bd97d14c09d4d2
Parmeet Singh <granthi.parmeet@gmail.com>
2026-08-04 05:11:20 +0530

    Update ids practice notes

--- ids.py
+++ ids.py
@@ -1,13 +1,260 @@
-x=10
-y=30
-print(id(x))
-print(type(y))
+# x=10
+# y=30
+# print(id(x))
+# print(type(y))
 
 
-a=[1,2]
-b=[1,2]
+# a=[1,2]
+# b=[1,2]
 
-print(a==b)
-print(a is b)
+# print(a==b)
+# print(a is b)
 
-print(type(None))
+# print(type(None))
+
+# x = int(float("3.15"))
+
+# print(type(x))
+
+# x = True
+# print(type(x))
+
+# print(10/3)
+# print(10*3)
+# print(10**3)
+# print(10//3)
+# print(10%3)
+# print(10)
+
+# x=100
+# y=100
+
+# print(x is y)
+
+# a=1000
+# b=1000
+
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+# h=543268791
+# p=543268791
+
+# print(h is p)
+
+# print(abs(-5))
+
+# print(pow(2,3))
+
+# print(round(3.4))
+
+# print(10/2)
+
+# print(type(None))
+
+# x = 10
+# y = "3.14"
+
+# print(float(x))
+# print(float(y))
+
+
+# print(10.5**2)
+# print(0.1+0.2)
+
+# import math
+
+# print(math.isclose(0.1+0.2, 0.3))
+
+# print(round(3.1456765,4))
+
+
+# a = 5
+# b = 5.0
+
+# print(a is b)
+
+
+# print(bool(1))
+# print(bool(-5))
+# print(bool(0))
+
+# print(bool(0.0))
+# print(bool(2.5))
+
+# print(bool([]))
+# print(bool({}))
+
+# print(bool(None))
+
+
+age = 20
+is_license = True
+
+# print (age<18 or is_license)
+
+
+# print(5<3 and 5<10)
+
+# print((5>3))
+
+# if len(users) > 0:
+
+# if users
+
+# if len(users) == 0:
+
+# if not users
+
+
+# def greet():
+#     print("hello")
+
+# result = greet()
+
+# print(result)
+
+
+# user = {
+#     "name":"Parmeet"
+# }
+
+# print(user.get("name"))
+
+# print(user.get("age"))
+
+# print(None is None)
+
+# def add():
+#     pass
+
+# # add()
+
+
+# prompt = """SUmmarize this given problem is simple words by telling me whats hapeening?
+# could you please guide me in detail"""
+
+# print(prompt)
+
+# print(prompt[10])
+
+# print(prompt[-1])
+
+
+# name = "Par"
+
+# print(len(name))
+
+# print(name[len(name)-0])
+
+
+# word ="python"
+
+# print(word[0])
+# print(word[-1])
+# print(word[len(word)-1])
+# print(word[-3])
+
+# print(len(word))
+
+# word = "Machine"
+
+# print(word[-2])
+
+# word1="Parmeet"
+# print(len(word1))
+# print(word1[0])
+# print(word1[-1])
+
+city = "Aurangabad"
+
+# print(city[0])
+# print(city[1])
+# print(city[-1])
+# print(city[-2])
+
+# print(len(city))
+
+# print("First Character:", city[0])
+
+# print(city[len(city)])
+
+# print(city[len(city)-26])
+
+# print(city[1:4])
+
+# print(len(city))
+
+# print(city[0:11])
+
+# print(city[:3])
+
+# print(city[3:])
+
+# result = print(city[5:2])
+
+# print(type(result))
+
+# print(id(result))
+
+
+# word = "Python"
+# print(id(word))
+
+# upper_word= word.upper()
+# # print(id(upper_word))
+# # print(upper_word)
+
+# # print(id(word))
+# # word = "J" + word[1:]
+
+# # print(word)
+# # print(id(word))
+
+# # word = "Python"
+
+# # word = word +"3"
+
+# # print(word)
+
+
+
+# word = "Parmeet Singh.  "
+
+# print(word.upper())
+
+# print(word)
+
+# print(word.lower())
+
+# print(word.strip())
+
+# print(id(word))
+
+# word = print(word.replace("Par","Jag"))
+
+# print(id(word))
+
+# word = "cat cat cat"
+
+# print(word.replace('cat', 'dog'))
+
+
+
+# new = "Hello"
+
+# print(len(new))
+# new = " hello "
+
+# print(new)
+# print(len(new))
+
+# new = new.strip()
+
+# print(new)
+
+# print(len(new))
+
+
+# name =
